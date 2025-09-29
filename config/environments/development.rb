04cd01b9de6ee90efd5118183725b2a933110503
samsolomon09 <solomonsam1994@gmail.com>
2025-09-29 16:05:17 +0530

    changes adding devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   config.action_view.annotate_rendered_view_with_filenames = true
 
+  #for proper URLs in mailer
+  config.action_mailer.default_url_options = {host: 'localhost', port: 3000 }
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
